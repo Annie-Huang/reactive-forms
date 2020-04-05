79e2e14aef54dda62fa0db91b68eff6bd57665e1
Annie Huang <annie.hn@gmail.com>
2020-04-05 17:42:40 +1000

    Update the service logic

--- src/app/registration.service.ts
+++ src/app/registration.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegistrationService {
 
-  constructor() { }
+  // tslint:disable-next-line:variable-name
+  _url = 'http://localhost:3000/enroll';
+
+  // tslint:disable-next-line:variable-name
+  constructor(private _http: HttpClient) { }
+
+  register(userData) {
+    return this._http.post<any>(this._url, userData);
+  }
 }
